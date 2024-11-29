3becbc51bf4ba42c0e123ab503a5ef6da33bf104
luis-domingues <luisdomingues117@gmail.com>
2024-11-29 10:35:50 -0300

    feat: implementar traducao para categorias em ptbr

--- src/main/java/br/com/alura/screenmatch/model/Categoria.java
+++ src/main/java/br/com/alura/screenmatch/model/Categoria.java
@@ -1,28 +1,39 @@
 package br.com.alura.screenmatch.model;
 
 public enum Categoria {
-    ACAO("Action"),
-    DRAMA("Drama"),
-    TERROR("Horror"),
-    SUSPENSE("Thriller"),
-    AVENTURA("Adventure"),
-    ANIMATION("Animation"),
-    ROMANCE("Romance"),
-    CURTO("Short"),
-    HISTORIA("History"),
-    MISTERIO("Mystery"),
-    COMEDIA("Comedy"),
-    CRIME("Crime");
+    ACAO("Action", "Ação"),
+    DRAMA("Drama", "Drama"),
+    TERROR("Horror", "Terror"),
+    SUSPENSE("Thriller", "Suspense"),
+    AVENTURA("Adventure", "Aventura"),
+    ANIMATION("Animation", "Animação"),
+    ROMANCE("Romance", "Ramance"),
+    CURTO("Short", "Curta"),
+    HISTORIA("History", "História"),
+    MISTERIO("Mystery", "Mistério"),
+    COMEDIA("Comedy", "Comédia"),
+    CRIME("Crime", "Crime");
 
     private String categoriaOmdb;
+    private String categoriaPortugues;
 
-    Categoria(String categoriaOmdb) {
+    Categoria(String categoriaOmdb, String categoriaPortugues) {
         this.categoriaOmdb = categoriaOmdb;
+        this.categoriaPortugues = categoriaPortugues;
     }
 
     public static Categoria fromString(String text) {
         for (Categoria categoria : Categoria.values()) {
             if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Nenhuma categoria encontrada para a string fornecida: " + text);
+    }
+
+    public static Categoria fromPortugues(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaPortugues.equalsIgnoreCase(text)) {
                 return categoria;
             }
         }
